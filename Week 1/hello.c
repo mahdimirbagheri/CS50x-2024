4475117d51322366b992ec1ad0a4e1e92a5765b8
S mahdimirbagheri <mahdimirbagheri85@gmail.com>
2024-04-24 22:41:44 +0330

    w h edit

---
+++ Week 1/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // getting a name from user
+    string name = get_string("What is your name? ");
+    // print a name from user
+    printf("hello, %s\n", name);
+}
+// Smahdimirbagheri
