a8817539e15b42f4fc7a7bcf78f0082656b3be35
S mahdimirbagheri <mahdimirbagheri85@gmail.com>
2024-04-24 22:47:12 +0330

    End Problem set 1

---
+++ mario-more.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height, j, i, f, e;
+
+    // get number from user
+    do
+    {
+        height = get_int("Enter A Number : ");
+    } while (height < 1 || height > 8);
+
+    // make a main for
+    for (i = 1; i <= height; i++)
+    {
+
+        // print space
+        for (f = 0; f < height - i; f++)
+        {
+            printf(" ");
+        }
+
+        // print # Ascending
+        for (j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+
+        // print # Descending
+        printf("  ");
+        for (e = 1; e <= i; e++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
