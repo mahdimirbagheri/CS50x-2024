82a31921850c8bfdcca4e5a344f7af9a383af0f8
S mahdimirbagheri <mahdimirbagheri85@gmail.com>
2024-04-25 10:32:30 +0330

    Week 2

---
+++ Week 2/Lab/readability.c
@@ -0,0 +1,49 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // TODO : Get a sentences from user
+    string text = get_string("Text: ");
+    float l = 0;
+    float w = 1;
+    float s = 0;
+    int n = strlen(text);
+    for (int i = 0; i < n; i++)
+    {
+        // TODO : Count letter
+        if ((text[i] >= 65 && text[i] <= 90) || (text[i] >= 97 && text[i] <= 122))
+        {
+            l++;
+        }
+        // TODO : Count word
+        if (text[i] == 32)
+        {
+            w++;
+        }
+        // TODO : Count sentences
+        if (text[i] == 46 || text[i] == 63 || text[i] == 33)
+        {
+            s++;
+        }
+    }
+    // TODO : Calculate the degree of difficulty
+    float L = 100 * (l / w);
+    float S = 100 * (s / w);
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+    if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}
---
+++ Week 2/Problem set 2/bulbs.c
@@ -0,0 +1,46 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+
+int main(void)
+{
+    // TODO : Get a massege from user
+    string m = get_string("Massege: ");
+    int n = strlen(m);
+    for (int i = 0; i < n; i++)
+    {
+        int binary[] = {0, 0, 0, 0, 0, 0, 0, 0};
+        int decimal = m[i];
+        int j = 0;
+        // TODO : Convert decimal to binery
+        while (decimal > 0)
+        {
+            binary[j] = decimal % 2;
+            decimal = decimal / 2;
+            j++;
+        }
+        for (int k = BITS_IN_BYTE - 1; k >= 0; k--)
+        {
+            print_bulb(binary[k]);
+        }
+        printf("\n");
+    }
+}
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
